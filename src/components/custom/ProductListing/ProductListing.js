cbfabd32ff485c4d34b5a5ad54a0571d092eb0ea
shisir-imarn-at-671004753547 <contact.shisirimran@gmail.com>
2025-05-24 15:45:58 +0600

    animation & ui fix

--- src/components/custom/ProductListing/ProductListing.js
+++ src/components/custom/ProductListing/ProductListing.js
@@ -1,13 +1,50 @@
 import SingleProductCard from "./SingleProductCard";
 import { Button } from "../../ui/button";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { MdOutlineErrorOutline } from "react-icons/md";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 function ProductListing(props) {
   const { title, results } = props;
   const [is_show_more, set_is_show_more] = useState(false);
   // Show only 4 products when is_show_more is false
   const displayedProducts = is_show_more ? results : results?.slice(0, 4);
+
+  // for animation
+  const containerRef = useRef(null);
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      const cards = gsap.utils.toArray(".product-card");
+
+      cards.forEach((card, index) => {
+        gsap.fromTo(
+          card,
+          {
+            opacity: 0,
+            y: 50,
+          },
+          {
+            opacity: 1,
+            y: 0,
+            duration: 0.6,
+            delay: index * 0.2,
+            ease: "power2.out",
+            scrollTrigger: {
+              trigger: card,
+              start: "top 80%",
+              end: "bottom 20%",
+              toggleActions: "play none none none",
+            },
+          }
+        );
+      });
+    }, containerRef);
+
+    return () => ctx.revert();
+  }, [displayedProducts]);
+
   return (
     <div>
       <div className="text-center">
@@ -15,9 +52,16 @@ function ProductListing(props) {
       </div>
 
       {displayedProducts?.length != 0 ? (
-        <div className="grid grid-cols-2 gap-3 md:gap-5 mt-3 md:mt-5 sm:grid-cols-2 lg:grid-cols-4">
+        <div
+          ref={containerRef}
+          className="grid grid-cols-2 gap-3 md:gap-5 mt-3 md:mt-5 sm:grid-cols-2 lg:grid-cols-4"
+        >
           {displayedProducts?.map((product, index) => (
-            <SingleProductCard key={index} product={product} />
+            <SingleProductCard
+              key={index}
+              product={product}
+              className="product-card"
+            />
           ))}
         </div>
       ) : (
